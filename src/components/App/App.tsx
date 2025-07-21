a4fccf3c97442152d3afe84bb8cf72b6eb14a037
Artem Dybov <dybov.artem.v@gmail.com>
2025-07-21 16:35:31 +0300

    Initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,58 @@
+import css from "./App.module.css";
+import Cafeinfo from "../CafeInfo/CafeInfo";
+import { useState } from "react";
+import { VoteType, Votes } from "../../types/votes";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes((prevVotes) => ({
+      ...prevVotes,
+      [type]: prevVotes[type] + 1,
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  // Обчислення загальної кількості голосів
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+  // Обчислення відсотка позитивних голосів
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
+  return (
+    <div className={css.app}>
+      <Cafeinfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
